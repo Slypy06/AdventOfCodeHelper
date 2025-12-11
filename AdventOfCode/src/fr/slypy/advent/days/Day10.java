1367da23797f6ed0f8ad3350def9112b71700a22
Enzo <enzo.m.7@free.fr>
2025-12-11 05:57:47 +0100

    Day10

--- AdventOfCode/src/fr/slypy/advent/days/Day10.java
+++ AdventOfCode/src/fr/slypy/advent/days/Day10.java
@@ -105,172 +105,17 @@ public class Day10 {
 		return "" + sum;
 		
 	}
-	
-	public static boolean isSolvable(List<Integer> goal) {
 
-		for(int l : goal) {
-			
-			if(l < 0)
-				return false;
-			
-		}
-		
-		return true;
-		
-	}
-	
-	public static boolean isSolved(List<Integer> goal) {
-
-		for(int l : goal) {
-			
-			if(l != 0)
-				return false;
-			
-		}
-		
-		return true;
-		
-	}
-	
-	public static void apply(List<Integer> coef, List<Integer> goal) {
-		
-		for(int l : coef) {
-			
-			goal.set(l, goal.get(l) - 1);
-			
-		}
-		
-	}
-	
-	public static int max(List<Integer> goal) {
-		
-		int max = 0;
-		
-		for(int l : goal) {
-			
-			if(l > max)
-				max = l;
-			
-		}
-		
-		return max;
-		
-	}
-	
-	/*public static int solve(List<List<Integer>> coefs, List<Integer> goal) {
-		
-		if(coefs.isEmpty())
-			return 0;
-		
-		if(!isSolvable(goal))
-			return 10_000_000;
-		
-		if(memo.containsKey(goal.toString() + coefs.size())) {
-			
-			return memo.get(goal.toString() + coefs.size());
-			
-		}
-		
-		if(coefs.size() == 1) {
-			
-			int tot = 0;
-			int m = max(goal);
-			
-			for(int i = 0; i <= m && !isSolved(goal); i++) {
-				
-				apply(coefs.get(0), goal);
-				tot++;
-				
-			}
-			
-			memo.put(goal.toString() + coefs.size(), isSolved(goal) ? tot : 10_000_000);
-			
-			return memo.get(goal.toString() + coefs.size());
-			
-		}
-		
-		int min = 10_000_000;
-		
-		for(int i = 0; i <= max(goal); i++) {
-
-			List<Integer> newGoal = new ArrayList<>(goal);
-			
-			for(int j = 0; j < i; j++)
-				apply(coefs.get(coefs.size() - 1), newGoal);
-			
-			if(!isSolvable(newGoal))
-				break;
-			
-			List<List<Integer>> newCoefs = new ArrayList<>(coefs);
-			newCoefs.remove(newCoefs.size() - 1);
-			int res = solve(newCoefs, newGoal) + i;
-
-			if(res >= 10_000_000)
-				continue;
-			
-			System.out.println("Potential : " + res);
-			
-			if(res < min) {
-				
-				min = res;
-				
-			}
-			
-		}
-		
-		memo.put(goal.toString() + coefs.size(), min);
-		
-		return min;
-		
-	}*/
-	
-	public static int solve(List<List<Integer>> coefs, List<Integer> goal) {
-	    String key = goal.toString();
-	    if (memo.containsKey(key))
-	        return memo.get(key);
-
-	    if (isSolved(goal)) {
-	        memo.put(key, 0);
-	        return 0;
-	    }
-
-	    if (!isSolvable(goal)) {
-	        memo.put(key, 10_000_000);
-	        return 10_000_000;
-	    }
-
-	    int best = 10_000_000;
-
-	    // try each vector once
-	    for (List<Integer> v : coefs) {
-
-	        List<Integer> newGoal = new ArrayList<>(goal);
-	        apply(v, newGoal);
-
-	        if (!isSolvable(newGoal)) continue;
-
-	        int sub = solve(coefs, newGoal);
-	        if (sub >= 10_000_000) continue;
-
-	        best = Math.min(best, sub + 1);
-	        
-	    }
-
-	    memo.put(key, best);
-	    return best;
-	    
-	}
-	
 	public static String part2(List<List<Integer>> joltages, List<List<List<Integer>>> buttons) {
-		
 		
 		long sum = 0;
 		
 		for(int i = 0; i < joltages.size(); i++) {
 			
-			memo = new HashMap<>();
+			List<Integer> voltages = joltages.get(i);
+			List<List<Integer>> btns = buttons.get(i);
 			
-			int min = solve(buttons.get(i), joltages.get(i));
+			int min = 0; //call python OR-Tools
 			
 			System.out.println(min);
 			sum += min;
